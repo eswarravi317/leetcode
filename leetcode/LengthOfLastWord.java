0cf1ed4512442b3fce9f41e077da85415c9bb02b
Eswar <109469319+eswarravi317@users.noreply.github.com>
2022-10-28 20:51:08 +0530

    Add files via upload

---
+++ leetcode/LengthOfLastWord.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LengthOfLastWord {
+	public int lengthOfLastWord(String s) {
+		String[] str = s.split(" ");
+		return str[str.length-1].length();
+    }
+	public static void main(String[] args) {
+		LengthOfLastWord obj = new LengthOfLastWord();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("String: ");
+		String str = sc.nextLine();
+		sc.close();
+		System.out.println(obj.lengthOfLastWord(str));
+	}
+}
